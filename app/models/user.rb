f436bff0115aa1e9beb9426885a01106ee34d79c
Aditya Bhutani <adityab@Aditya-ka-MacBook-Pro.local>
2024-02-16 07:20:23 +0530

    Use favorite_teachers instead of user.teachers.favorites. Added scoped associations to models/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :enrollments
     has_many :programs, through: :enrollments
     has_many :teachers, -> { distinct }, through: :enrollments, class_name: "User"
+
+    has_many :favorite_enrollments, -> { where(favorite: true) }, class_name: "Enrollment"
+    has_many :favorite_teachers, through: :favorite_enrollments, source: :teacher
 end
