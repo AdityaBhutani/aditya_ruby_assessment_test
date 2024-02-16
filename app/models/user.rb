47adb4294dcdeaaf1c9944c527c49b04ce9465ea
Aditya Bhutani <adityab@Aditya-ka-MacBook-Pro.local>
2024-02-16 07:18:26 +0530

    Add enum kind and associations to models/user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+    enum kind: %i[student teacher student_teacher]
+
+    has_many :enrollments
+    has_many :programs, through: :enrollments
+    has_many :teachers, -> { distinct }, through: :enrollments, class_name: "User"
 end
